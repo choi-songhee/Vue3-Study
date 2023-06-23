76faa41fc23d79c3712937f4390bf71f6125053b
songhee <ssonghee15@gmail.com>
2023-06-23 17:55:36 +0900

    0623

---
+++ src/ReactiveBase.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<button @click="increment">Click {{ state.count }}</button>
+		<button @click="increment">Click {{ state.deep.count }}</button>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+	setup() {
+		// 반응형 객체로 만들어줌
+		const state = reactive({
+			count: 0,
+			deep: {
+				count: 0,
+			},
+		});
+		const increment = () => {
+			state.count++;
+			state.deep.count++;
+		};
+
+		return {
+			state,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
