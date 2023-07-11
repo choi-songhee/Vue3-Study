2d378ccd5219e3dc62692f7d873145532340cbe9
songhee <ssonghee15@gmail.com>
2023-07-11 17:37:43 +0900

    0711 study

---
+++ src/vModel.vue
@@ -0,0 +1,82 @@
+<template>
+	<div>
+		<h2>input value</h2>
+		<!-- <input type="text" v-model.lazy="inputValue" /> -->
+		<!-- <input type="text" v-model.number="inputValue" /> -->
+		<!-- trim: 앞뒤 공백제거 -->
+		<input type="text" v-model.trim="inputValue" />
+		<div>{{ inputValue }}</div>
+		<div>{{ typeof inputValue }}</div>
+
+		<h2>textarea value</h2>
+		<textarea v-model="textareaValue"></textarea>
+		<div>{{ textareaValue }}</div>
+
+		<h2>checkbox value</h2>
+		<label for="checkbox">{{ checkboxValue }}</label>
+		<input
+			type="checkbox"
+			id="checkbox"
+			v-model="checkboxValue"
+			true-value="Yes"
+			false-value="No"
+		/>
+		<h3>checkbox values</h3>
+		<label>
+			<input type="checkbox" value="html" v-model="checkboxValues" />
+			Html
+		</label>
+		<label>
+			<input type="checkbox" value="css" v-model="checkboxValues" />
+			Css
+		</label>
+		<label>
+			<input type="checkbox" value="script" v-model="checkboxValues" />
+			Script
+		</label>
+		<div>{{ checkboxValues }}</div>
+
+		<h2>radio value</h2>
+		<label>
+			<input type="radio" name="type" value="O" v-model="radioValue" />
+			O형
+		</label>
+		<label>
+			<input type="radio" name="type" value="A" v-model="radioValue" />
+			A형
+		</label>
+
+		<h2>select value</h2>
+		<select v-model="selectValue">
+			<option value="html">html</option>
+			<option value="css">css</option>
+			<option value="javascript">javascript</option>
+		</select>
+		<div>{{ selectValue }}</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup() {
+		const inputValue = ref('');
+		const textareaValue = ref('');
+		const checkboxValues = ref([]);
+		const checkboxValue = ref('Yes');
+		const radioValue = ref('O');
+		const selectValue = ref('html');
+
+		return {
+			inputValue,
+			textareaValue,
+			checkboxValue,
+			radioValue,
+			selectValue,
+			checkboxValues,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
