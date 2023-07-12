f780fff5bcf2006fc21bfdfc8da0d9c149bffe8c
songhee <ssonghee15@gmail.com>
2023-07-12 17:41:32 +0900

    0712 study

---
+++ src/watchEffect.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<form @submit.prevent>
+			<input v-model.lazy="title" type="text" placeholder="title" />
+			<textarea v-model.lazy="contents" placeholder="contents"></textarea>
+			<button @click="save(title, contents)">Save</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+export default {
+	setup() {
+		const title = ref('');
+		const contents = ref('');
+		const save = (title, contents) => {
+			console.log(`저장: ${title}, ${contents}`);
+		};
+
+		// watch 보다 덜 명시적
+		watchEffect(() => {
+			save(title.value, contents.value);
+			console.log(title.value);
+			console.log(contents.value);
+		});
+
+		return {
+			title,
+			contents,
+			save,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/watchImmediate.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<p>{{ reverseMessage }}</p>
+	</div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+export default {
+	setup() {
+		const message = ref('hello');
+		const reverseMessage = ref('');
+
+		const reverseFunction = () => {
+			reverseMessage.value = message.value.split('').reverse().join('');
+		};
+
+		// watch(message, reverseFunction, {
+		// 	immediate: true, // 최초 한번 즉시실행
+		// });
+		watch(message, reverseFunction);
+		reverseFunction();
+
+		return {
+			message,
+			reverseMessage,
+			reverseFunction,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
